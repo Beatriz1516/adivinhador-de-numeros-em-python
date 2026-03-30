dad3f61e069e230cbc4d7b7984c09b07159b91ca
Beatriz1516 <gomes.pessoa@escola.pr.gov.br>
2026-03-30 09:45:20 -0300

    Foi adicionada uma forma visual de mostrar quantas letras tem a palavra secreta. Foi adicionada um retorno de quantas letras foram acertadas, em que posição e quais.

--- forca.py
+++ forca.py
@@ -3,27 +3,33 @@ def jogar():
     print("******************************************")
     print("********Bem-vindo ao jogo da forca********")
     print("******************************************")
-    print("Fim de jogo")
     
 #definição da palavra secreta 
     palavra_secreta = "paracetamol"
+    letras_acertadas = ["_","_","_","_","_","_","_","_","_","_","_"]
+    
 #definição das variáveis de acerto ou enforco
     enforcou = False
     acertou = False
+
+    print(letras_acertadas)
 
 #definição do laço de repetição para continuar jogando
     while(not acertou and not enforcou):
         chute = input("Qual a letra?")
         chute = chute.strip()
         
-        index = 1
+        index = 0
 #utiliza o for para verificar a sequência (de caracteres)
         for letra in palavra_secreta:
             if(chute.upper() == letra.upper()):
-                print("A letra {1} tem na posição {0}.".format(index, letra.upper()))
+              letras_acertadas[index]= letra
             index = index + 1      
-                      
-        print("jogando...")
+        
+        print(letras_acertadas)
+#retorna quantas letras ainda não foram encontradas
+        letras_faltando = (letras_acertadas.count("_"))
+        print("Ainda faltam acertar {} letras".format(letras_faltando))
 
 #quando o arquivo for executar fora da função ele define name como main, e ele pode ser executado. Mas, quando o arquivo jogos for executado a pasta só será importada.
 if(__name__ == "__main__"):
