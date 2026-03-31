f8133fe68952ecc1f6757451667781e74b03b6e1
Beatriz1516 <gomes.pessoa@escola.pr.gov.br>
2026-03-31 16:13:19 -0300

    Foi adicionada a contagem de erros. Foi colocada todas as letras de chute e da palavra secreta para maiúscula. Foi adicionado um fim caso todas as letras são acertadas e/ou se em seis tentativas o jogador não acertar.

--- forca.py
+++ forca.py
@@ -4,32 +4,48 @@ def jogar():
     print("********Bem-vindo ao jogo da forca********")
     print("******************************************")
     
-#definição da palavra secreta 
-    palavra_secreta = "paracetamol"
+#definição da palavra secreta e das letras acertadas
+    palavra_secreta = "paracetamol".upper()
     letras_acertadas = ["_","_","_","_","_","_","_","_","_","_","_"]
     
-#definição das variáveis de acerto ou enforco
+#definição das variáveis de acerto, erros e enforco
     enforcou = False
     acertou = False
+    erros= 0
 
     print(letras_acertadas)
 
-#definição do laço de repetição para continuar jogando
+#definição da repetição do jogo, de entrada do chute e verificação de acerto
     while(not acertou and not enforcou):
-        chute = input("Qual a letra?")
-        chute = chute.strip()
+        chute = input("Qual a letra?").upper()
+        chute = chute.strip().upper()
         
-        index = 0
-#utiliza o for para verificar a sequência (de caracteres)
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-              letras_acertadas[index]= letra
-            index = index + 1      
-        
+        if (chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    letras_acertadas[index]= letra
+                index += 1
+        else: 
+            erros += 1    
+    
+#se não tiver mais letras a serem encontradas, o jogador finaliza por acerto
+        acertou = "_" not in letras_acertadas
+        if (acertou):
+            print("Você acertou a palavra secreta!")
+            break
         print(letras_acertadas)
-#retorna quantas letras ainda não foram encontradas
+
+#repetição de quais letras falta descobrir, se erros < 6 (continua), se erros = 6 (enforcado)
         letras_faltando = (letras_acertadas.count("_"))
-        print("Ainda faltam acertar {} letras".format(letras_faltando))
+        if (erros == 1):
+            print("Você já errou {1} vez, faltam {2} tentativas e {0} letras".format(letras_faltando, erros, 6-erros))
+        elif (erros < 6):
+            print("Você já errou {1} vezes, faltam {2} tentativas e {0} letras".format(letras_faltando, erros, 6-erros))
+        else:
+            print("Você não conseguiu acertar a palavra secreta! Era {} :(".format(palavra_secreta))
+            if (print("Fim do jogo")):
+                break
 
 #quando o arquivo for executar fora da função ele define name como main, e ele pode ser executado. Mas, quando o arquivo jogos for executado a pasta só será importada.
 if(__name__ == "__main__"):
